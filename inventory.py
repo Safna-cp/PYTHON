f1d462d17bb64a1824095a353d1fc5aaf91387a7
Fathima Safna <safnaclt123@gmail.com>
2025-08-21 16:53:46 +0530

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,49 @@
+# fruits=['apple','mango','strawberry','peach','kiwi','banana']
+# print(fruits)
+
+
+
+# print(fruits[0])
+# print(fruits[1:3])
+
+
+
+# fruits.append('orange')
+# print(fruits)
+
+
+
+# if "mango" in fruits:
+#       print("is in the fruit list.")
+# else:
+#     print("is not in the fruit list.")
+
+# fruits.remove("peach")
+# print(fruits)
+
+
+
+
+# while True:
+#     print("\n 1.Add  2.View  3.Exit")
+#     choice=input("enter choice:")
+
+#     if choice == "1":
+#         item = input("Add item:")
+#         fruits.append(item)
+#         print("Added:", fruits)
+#     elif choice =="2":
+#         for i,it in enumerate(fruits,start=1):
+#             print(i,it)
+#     else:
+#         exit()
+    
+
+
+
+fruits= [
+    {"name": "apple", "price": 2.5, "quantity": 4},
+    {"name": "banana", "price": 1.2, "quantity": 10},
+    {"name": "orange", "price": 3.0, "quantity": 3}
+]
+print(fruits)
